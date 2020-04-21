72473254777e6b903e9e7e1674994a5d6824d335
KelliePetersen <kelliepetersen@outlook.com>
2020-04-21 10:07:21 -0600

    add style jsx to navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,10 +2,31 @@ import Link from 'next/link';
 
 const Navbar = () => {
   return (
-    <ul>
-      <li><Link href="/"><a>Home</a></Link></li>
-      <li><Link href="/about"><a>About</a></Link></li>
-    </ul>
+    <div>
+      <ul>
+        <li><Link href="/"><a>Home</a></Link></li>
+        <li><Link href="/about"><a>About</a></Link></li>
+      </ul>
+
+      <style jsx>{`
+        ul {
+          background: #333;
+          color: #fff;
+          list-style: none;
+          display: flex;
+        }
+
+        ul li {
+          font-size: 18px;
+          margin-right: 20px;
+        }
+
+        ul li a {
+          color: #fff;
+          text-decoration: none;
+        }
+      `}</style>
+    </div>
   )
 }
 
