37e91142f8a3d985ee130a5307a22a2e7fa2621e
KelliePetersen <kelliepetersen@outlook.com>
2020-04-21 10:10:03 -0600

    add metadata and styling to layout component

--- components/layout.js
+++ components/layout.js
@@ -1,10 +1,24 @@
+import Head from 'next/head';
 import Navbar from '../components/Navbar';
 
 const Layout = props => {
   return (
     <div>
+      <Head>
+        <title>Bitzbytes</title>
+      </Head>
       <Navbar />
-      {props.children}
+      <div className="container">
+        {props.children}
+      </div>
+
+      <style jsx>{`
+        .container {
+          width: 960px;
+          max-width: 80%;
+          margin: 50px auto;
+        }
+      `}</style>
     </div>
   )
 }
